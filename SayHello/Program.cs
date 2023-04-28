b9c70db099bd4a9f50a95a6492ed4df086eaba43
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-04-27 21:56:35 -0700

    Print -> Say Hello
    Hien thi loi chao kiem voi mot ten ai do

---
+++ SayHello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap Ten cua ban: ");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Hello my frend "+ Name);
+        }
+    }
+}
